770f320a98980f3cdca6bb7a09a4fdb8adce9279
minyeongnam <minyn0106@gmail.com>
2022-04-12 10:44:26 +0900

    feat: study 6.4~6.5

--- 5-generic/5-3-constrains.ts
+++ 5-generic/5-3-constrains.ts
@@ -8,3 +8,50 @@ class FullTimeEmployee implements Employee {
   }
   workFullTime() {}
 }
+
+class PartTimeEmployee implements Employee {
+  pay() {
+    console.log("part Time");
+  }
+  workPartTime() {}
+}
+
+// 세부적인 타입을 인자로 받아서 정말 추상적인 타입으로 다시 리턴하는 함수는 💩💩💩
+function payBad(employee: Employee): Employee {
+  employee.pay();
+  return employee;
+}
+
+const ellie = new FullTimeEmployee();
+const bob = new PartTimeEmployee();
+ellie.workFullTime();
+bob.workPartTime();
+
+const ellieAfterPayBad = payBad(ellie);
+const bobAfterPayBad = payBad(bob);
+
+function pay<T extends Employee>(employee: T): T {
+  employee.pay();
+  return employee;
+}
+
+const ellieAfterPay = pay(ellie);
+const bobAfterPay = pay(bob);
+
+const obj = {
+  name: "ellie",
+  age: 20,
+};
+
+const obj2 = {
+  animal: "🐹",
+};
+
+function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+console.log(getValue(obj, "name")); // ellie
+console.log(getValue(obj, "age")); // 20
+console.log(getValue(obj2, "animal")); // 🐹
+// console.log(getValue(obj2, "score")); // error
